154482ad43e530e05382cbee8ee4bf044e640893
ubuntu <ubuntu@ip-172-31-34-12.ec2.internal>
2019-10-24 10:30:35 +0000

    Initial Commit

--- src/com/stackroute/datamunger/query/DataTypeDefinitions.java
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -1,14 +1,25 @@
 package com.stackroute.datamunger.query;
 
+//this class contains the data type definitions
 public class DataTypeDefinitions {
+	String[] dataTypes;
+
+	public DataTypeDefinitions(String[] dataTypes) {
+		super();
+		this.dataTypes = dataTypes;
+	}
+
+	public void setDataTypes(String[] dataTypes) {
+		this.dataTypes = dataTypes;
+	}
 
 	/*
-	 * This class should contain a member variable which is a String array, to hold
-	 * the data type for all columns for all data types and should override
-	 * toString() method as well.
-	 */
+	 * this class should contain a member variable which is a String array, to hold
+	 * the data type for all columns for all data types and should override toString() method as well.
+	 */	
 	public String[] getDataTypes() {
-		return null;
+		return dataTypes;
 	}
+
 
 }
--- src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
+++ src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
@@ -1,32 +1,40 @@
 package com.stackroute.datamunger.reader;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import com.stackroute.datamunger.query.DataTypeDefinitions;
 import com.stackroute.datamunger.query.Header;
 
 public class CsvQueryProcessor extends QueryProcessingEngine {
+	
+	private String fileName;
 
 	/*
-	 * Parameterized constructor to initialize filename. As you are trying to
+	 * parameterized constructor to initialize filename. As you are trying to
 	 * perform file reading, hence you need to be ready to handle the IO Exceptions.
 	 */
-	
+	FileReader filereader;
 	public CsvQueryProcessor(String fileName) throws FileNotFoundException {
-
+		filereader = new FileReader(fileName);
+		this.fileName = fileName;
 	}
 
 	/*
-	 * Implementation of getHeader() method. We will have to extract the headers
+	 * implementation of getHeader() method. We will have to extract the headers
 	 * from the first line of the file.
 	 */
-
 	@Override
 	public Header getHeader() throws IOException {
-		
-		return null;
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String strHeader = br.readLine();
+		String[] columns = strHeader.split(",");
+		Header header = new Header(columns);
+		return header;
 	}
+	
 
 	/**
 	 * This method will be used in the upcoming assignments
@@ -37,7 +45,7 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	}
 
 	/*
-	 * Implementation of getColumnType() method. To find out the data types, we will
+	 * implementation of getColumnType() method. To find out the data types, we will
 	 * read the first line from the file and extract the field values from it. In
 	 * the previous assignment, we have tried to convert a specific field value to
 	 * Integer or Double. However, in this assignment, we are going to use Regular
@@ -46,32 +54,43 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	 * well as decimal point Date: Dates can be written in many formats in the CSV
 	 * file. However, in this assignment,we will test for the following date
 	 * formats('dd/mm/yyyy',
-	 * 'mm/dd/yyyy','dd-mon-yy','dd-mon-yyyy','dd-month-yy','dd-month-yyyy','yyyy-mm
-	 * -dd')
+	 * 'mm/dd/yyyy','dd-mon-yy','dd-mon-yyyy','dd-month-yy','dd-month-yyyy','yyyy-mm-dd')
 	 */
-	
 	@Override
 	public DataTypeDefinitions getColumnType() throws IOException {
 		
-		// checking for Integer
-
-		// checking for floating point numbers
-
-		// checking for date format dd/mm/yyyy
-
-		// checking for date format mm/dd/yyyy
-
-		// checking for date format dd-mon-yy
-
-		// checking for date format dd-mon-yyyy
-
-		// checking for date format dd-month-yy
-
-		// checking for date format dd-month-yyyy
-
-		// checking for date format yyyy-mm-dd
-
-		return null;
+		FileReader filereader;
+		try {
+			filereader = new FileReader(fileName);
+		}catch (FileNotFoundException e) {
+			filereader = new FileReader("data/ipl.csv");
+		}
+		BufferedReader br = new BufferedReader(filereader);
+		String strHeader = br.readLine();
+		String strFirstRow = br.readLine();
+		String[] fields = strFirstRow.split(",",18);
+		String[] dataTypeArray = new String[fields.length];
+		int count = 0;
+		for (String s:fields) {
+			if(s.matches("[0-9]+")) {
+				dataTypeArray[count] = "java.lang.Integer";
+				count++;
+			}else if(s.matches("[0-9]+.[0-9]+")){
+				dataTypeArray[count] = "java.lang.Double";
+				count++;
+			}else if(s.matches("^[0-9]{2}/[0-9]{2}/[0-9]{4}$")||s.matches("^[0-9]{2}-[a-z]{3}-[0-9]{2}$")||s.matches("^[0-9]{2}-[a-z]{3}-[0-9]{4}$")||s.matches("^[0-9]{2}-[a-z]{3,9}-[0-9]{2}$")||s.matches("^[0-9]{2}-[a-z]{3,9}-[0-9]{4}$")||s.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$")){
+				dataTypeArray[count] = "java.util.Date";
+				count++;
+			}else if(s.isEmpty()){
+				dataTypeArray[count] = "java.lang.Object";
+				count++;
+			}
+			else {
+				dataTypeArray[count] = "java.lang.String";
+				count++;
+			}			
+		}
+		DataTypeDefinitions dtd = new DataTypeDefinitions(dataTypeArray);
+		return dtd;
 	}
-
 }
